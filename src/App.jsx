bed14ebf723f8e352fb4e78c1fc2771eee3e5b01
Byron <Garciabyron175@gmail.com>
2026-08-19 16:23:48 -0700

    NavBar change to Sidebar Pill Nav to resemble POS system rather than a website

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar   from "./components/Navbar";
+import PillSideNav   from "./components/PillSideNav";
 import Hero     from "./components/Hero";
 import Features from "./components/Features";
 import Pricing  from "./components/Pricing";
@@ -6,8 +6,8 @@ import Foooter  from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
-      <Navbar />
+    <div className="min-h-screen bg-cornsilk text-black overflow-hidden">
+      <PillSideNav />
       <Hero />
       <Features />
       <Pricing />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,18 @@
 export default function Hero() {
     return (
-        <div>
-            
-        </div>
+        <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-16 lg:px-8 overflow-hidden">
+            <div className="">
+
+            </div>
+            <div>
+                <div>
+                    <div>
+                        {/* Hero Content */}
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
